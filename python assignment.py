5c8dc8f4238a6f167cb21a4f8141bc7f542d5a27
Devika Sangpal <73119607+devika112@users.noreply.github.com>
2020-10-26 15:31:50 +0530

    Add files via upload

---
+++ python assignment.py
@@ -0,0 +1,52 @@
+print("""NAME = DEVIKA SANGPAL
+PLACE= PUNE
+STREAM = BIOTECHNOLOGY
+COLLEGE= INSTITUTE OF BIOINFORMATICS AND BIOTECHNOLOGY""")
+
+print("ans 2= def is a python function so it cannot be used as an identifier")
+
+print("answer 3= Indentation Error")
+
+print("""answer 4 = single and double quotes in python are used for only string.
+double and single quotes can be used interchangebly in string. 
+whereas triple quotes are used to print multiple lines without using the print function everytime""")
+
+print("answer 5")
+print(19/2, 27/4, 100/3, 20/5)
+
+print(""" answer 6= in the above arithmatic operations , arithmatic operator - / i.e. division operator which divides
+ the left hand side operand by right hand side operand""")
+
+print("answer 7")
+A='Python is an interactive language'
+print(A[5], A[11], A[13], A[18], A[23])
+print("made use of arrey indexing and slice operator")
+
+print("answer 8")
+
+C= ['Greece', 'Japan', 'Portugal','Russia',' Norway']
+print(C[3])
+print(C[0],C[1])
+print(C[3],C[4])
+C[0]= 'Germany','Argentina','India','China','Turkey'
+print(C)
+
+
+print("answer 9")
+Tuple =('sparrow','mango','banana')
+print(Tuple)
+
+print("answer 10")
+print('tuple with different datatypes')
+Tuple =('sparrow',12.3,'mango',100)
+print(Tuple)
+
+print("answer 11")
+print('objects cannot be updated or added in a tuple')
+
+print('answer 12, dictionary')
+dict_devika = {'Name': 'Devika','Age':19,'stream':'Biotechnology'}
+print(dict_devika.keys())
+print(dict_devika.values())
+
+
